b34f9f9155707f49af156c1313284c2fe06298b2
VKD07 <116175769+VKD07@users.noreply.github.com>
2023-02-04 16:39:21 +0400

    added seasonal Change animation

--- LifeOfTree/Assets/Scripts/SeasonChanges.cs
+++ LifeOfTree/Assets/Scripts/SeasonChanges.cs
@@ -1,31 +1,105 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SeasonChanges : MonoBehaviour
 {
     [SerializeField] string[] seasons;
     float timeOfChange;
-    
+
+    [SerializeField] GameObject [] seasonObjects;
+    [SerializeField] TextMeshProUGUI uiText;
+    public int randomSeason;
+
+    [SerializeField] List<string> seasonNames;
+
+ 
+
+    Animator anim;
+
+
     void Start()
     {
-        while (true)
-        {
+        
             StartCoroutine(SetTheSeason());
-        }
+        anim = GetComponent<Animator>();
+       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-
-
+     
+    
     }
+
+ 
 
     IEnumerator SetTheSeason()
     {
-        timeOfChange = Random.Range(15, 30);
-        yield return new WaitForSeconds(timeOfChange);
+        timeOfChange = Random.Range(5, 10);
+        while (true)
+        {
+            
+            yield return new WaitForSeconds(timeOfChange);
+
+
+            randomSeason = Random.Range(0, seasonNames.Count);
+
+            changeSeasonText();
+            AddAndRemove();
+            
+            anim.SetTrigger("changeSeason");
+
+
+
+
+        }
     }
+
+    void AddAndRemove()
+    {
+        if (seasonNames.Count > 0)
+        {
+            seasonNames.Remove(seasonNames[randomSeason]);
+        }
+
+        if (seasonNames.Count <= 0)
+        {
+            seasonNames.Add("Spring");
+            seasonNames.Add("Summer");
+            seasonNames.Add("Winter");
+        }
+
+    }
+
+    private void changeSeasonText()
+    {
+        if (seasonNames[randomSeason].Contains("Spring"))
+        {
+            
+            seasonObjects[0].SetActive(true);
+            seasonObjects[1].SetActive(false);
+            seasonObjects[2].SetActive(false);
+
+        }else if(seasonNames[randomSeason].Contains("Summer"))
+        {
+           
+            seasonObjects[0].SetActive(false);
+            seasonObjects[1].SetActive(true);
+            seasonObjects[2].SetActive(false);
+        }
+        else if (seasonNames[randomSeason].Contains("Winter"))
+        {
+        
+            seasonObjects[0].SetActive(false);
+            seasonObjects[1].SetActive(false);
+            seasonObjects[2].SetActive(true);
+        }
+    }
+
+
 }
